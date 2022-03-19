1d4d6f3f82fe288511bd85fda3bc4ac49c4dcfd6
Han <hanescargot@gmail.com>
2022-03-19 17:26:51 +0900

    # driver

---
+++ ProductDto.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+@dataclass
+class Product:
+    state: str = "신상품"
+    categoryCode: int = 50006675
+    name: str = "아이템 명 오류"
+    price : int = 1000000
+    count : int = 1
+    asMsg : str = "제품 상세 내역 조회"
+    asPhoneNumber : str = "01044890709"
+    mainImgFileName : str = "snail.png"
+    subImgFileName: str = "snail.png"
+    imgUrl : str = "test"
+    bugase : str = "과세상품"
+    kidAble : str = "Y"
+    reviewVisible : str = "Y"
+    countryCode : str = "0200037"
+    shipCompany : str = "DogPy"
+    shipDuplicated : str = "N"
+    delivery : str = "택배‚ 소포‚ 등기"
+    deliveryPriceType : str = "수량별" #무료 조건부무료 유료
+    defaultDeliveryPrice : int = 60000
+    deliveryPayment : str = "선결제"
+    deliveryPriceItemCount : int = 1
+    deliveryRefund : int = 60000
+    deliveryExchange : int = 60000
+    optionType : str = "단독형"
+    optionCategory : str = "" #띄어쓰기로 구분
+    optionName : str = "" # 이름은,로 카테고리는 줄바꿈으로 구반
+    optionPrice : str = "" #,로 구분 없으면 0원
+    optionCount : str = "" #재고수량 ,로 구분
+    vip : str = "N"
+
+
+
+
--- test.py
+++ test.py
@@ -1,67 +1,160 @@
+import datetime
+import time
+
 import requests
 from bs4 import BeautifulSoup as soup, BeautifulSoup
 from requests_html import AsyncHTMLSession
 import pyppdf.patch_pyppeteer
 import asyncio
+import re
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from ProductDto import Product
+import os
+import pandas as pd
 target = ["title", "itemDetailUrl", "imagePath"]
+from dataclasses import asdict, dataclass
 
 
-async def main():
-    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
-    #HTML = 'https://de.aliexpress.com/item/1005003462345537.html?spm=a2g0o.productlist.0.0.5b5d7f3eP7WT2w&algo_pvid=acc27bba-a787-46dd-a4f5-48ffc27e1706&aem_p4p_detail=2022031107115612782986930137560023687702&algo_exp_id=acc27bba-a787-46dd-a4f5-48ffc27e1706-4&pdp_ext_f=%7B%22sku_id%22%3A%2212000025899459854%22%7D&pdp_pi=-1%3B30948.0%3B-1%3B-1%40salePrice%3BKRW%3Bsearch-mainSearch'
-    HTML = "https://ko.aliexpress.com/item/1005001299588690.html?spm=a2g0o.new_user_benefits.0.0.49f9BcK2BcK2G4&pdp_ext_f=%7B%22ship_from%22:%22CN%22,%22sku_id%22:%2212000025048220927%22%7D&scm=1007.34914.273009.0&scm_id=1007.34914.273009.0&scm-url=1007.34914.273009.0&pvid=7e2addeb-4a47-4ecd-ae7a-5c7d6e056971&utparam=%257B%2522process_id%2522%253A%2522401%2522%252C%2522x_object_type%2522%253A%2522product%2522%252C%2522pvid%2522%253A%25227e2addeb-4a47-4ecd-ae7a-5c7d6e056971%2522%252C%2522belongs%2522%253A%255B%257B%2522floor_id%2522%253A%252222234693%2522%252C%2522id%2522%253A%2522782032%2522%252C%2522type%2522%253A%2522dataset%2522%257D%252C%257B%2522id_list%2522%253A%255B%25221000094891%2522%255D%252C%2522type%2522%253A%2522gbrain%2522%257D%255D%252C%2522pageSize%2522%253A%252224%2522%252C%2522language%2522%253A%2522ko%2522%252C%2522scm%2522%253A%25221007.34914.273009.0%2522%252C%2522countryId%2522%253A%2522KR%2522%252C%2522tpp_buckets%2522%253A%252221669%25230%2523265320%252379_21669%25234190%252319159%2523107_24914%25230%2523273009%252318%2522%252C%2522x_object_id%2522%253A%25221005001299588690%2522%257D"
-    asession = AsyncHTMLSession()
-    request = await asession.get(
-            HTML
-    )
-    await request.html.arender()
+
+
+
+
+async def getDescripImg():
+    HTML = 'https://de.aliexpress.com/item/1005003624117637.html'
+    driver = webdriver.Chrome('C:/Users/pyrio/pythonProject/shopping_macro/chromedriver_win32/chromedriver.exe')
+    driver.get(HTML)
+
+    # 스크롤 기능
+    driver.execute_script('window.scrollTo(0, 800);')
+    time.sleep(1)
+    print('====success=====')
+    items = driver.find_elements_by_css_selector('#product-description>div>p>img')
+
+    for i in items:
+        print(i.get_attribute("src"))
+
+    await driver.close()
+    return
+
+async def main(type, maginPercent, categoryCode):
+    print(type)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+    HTML = 'https://de.aliexpress.com/item/1005003624117637.html'
 
     try:
-        h1 = request.html.find('h1')
+        response = requests.get(HTML, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')#'html.parser'    'lxml'
+        print(soup)
+        print("===================================")
+
+
+        asession = AsyncHTMLSession()
+        request = await asession.get(
+            HTML
+        )
+        await request.html.arender()
+        html = request.html
+
+        #start
+        product = Product()
+        product.categoryCode = categoryCode
+
+
+        h1 = html.find("title")
         title = h1[0].text
         print(title)
+        product.name = title
 
-        rating_count = request.html.find('.product-reviewer-reviews')
+        rating_count = html.find('.product-reviewer-reviews')
         rating = rating_count[0].text
         print(rating)
-
-        price = request.html.find('.uniform-banner-box-price')
-        priceStr = price[0].text
-        print(priceStr)
+        # .uniform-banner-box-price
+        # .product-price-value
+        price = html.find('.product-price-value')[0].text
+        print(price)
+        price = int(price.replace(",","").split(" ")[-1])
+        product.price= round(price + price/100*maginPercent, -2)
+        print(price)
+        print(product.price)
 
         # '#root>div>.product-main>div>.product-info>.product-sku>div>div:nth-child(1)>ul li>div' //국가
         # '#root>div>.product-main>div>.product-info>.product-sku>div>div:nth-child(2)>ul li>div' // 옵션 이미지
         # .product-info>.product-sku>div>.sku-property:nth-child(3)>ul li:nth-child(1)>div //옵션 텍스트
-        property = '.product-info>.product-sku>div>.sku-property:nth-child(3)>ul li>.sku-property-text>span'
-        t = request.html.find(property)
-        print(t)
-        for x in t:
-            print(x.text)
-            print("-----")
+        # '.product-info>.product-sku>div>.sku-property:nth-child(3)>ul li>.sku-property-text>span'
+        propertyPath = '.product-info>.product-sku li>.sku-property-text>span'
+        property = html.find(propertyPath)
+        for p in property:
+            print(p.text)
+        print("===pass property==")
 
-        await request.html.browser.close()
+
+        getDescripImg()
+
+        defPath = "C:/Users/pyrio/pythonProject/shopping_macro/"
+        # df = pd.json_normalize(asdict(Product()))
+        columns = {'상품상태':product.state,
+                      '카테고리ID': product.categoryCode,
+                      '상품명':product.name,
+                      '판매가':product.price,
+                      '재고수량':product.count,
+                      'A/S 안내내용':product.asMsg,
+                      'A/S 전화번호':product.asPhoneNumber,
+                      '대표 이미지 파일명':product.mainImgFileName,
+                      '추가 이미지 파일명':product.subImgFileName,
+                      '상품 상세정보':product.imgUrl,
+                      '부가세':product.bugase,
+                      '미성년자 구매':product.kidAble,
+                      '구매평 노출여부':product.reviewVisible,
+                      '원산지 코드':product.countryCode,
+                      '수입사':product.shipCompany,
+                      '복수원산지 여부':product.shipDuplicated,
+                      '배송방법':product.delivery,
+                      '배송비 유형':product.deliveryPriceType,
+                      '기본배송비':product.defaultDeliveryPrice,
+                      '배송비 결제방식':product.deliveryPayment,
+                      '수량별부과-수량':product.deliveryPriceItemCount,
+                      '반품배송비':product.deliveryRefund,
+                      '교환배송비':product.deliveryExchange,
+                      '옵션형태':product.optionType,
+                      '옵션명':product.optionCategory,
+                      '옵션값':product.optionName,
+                      '옵션가':product.optionPrice,
+                      '옵션 재고수량':product.optionCount,
+                      '스토어찜회원 전용여부':product.vip
+        }
+        if(os.path.isfile(defPath+"result.xlsx")):
+            print("excel from result")
+            df = pd.read_excel(defPath+"result.xlsx",engine='openpyxl')
+        else:
+            print("excel from template")
+            df = pd.read_excel(defPath+"ProductTemplate.xls", engine='openpyxl')
+
+
+
+        result = df.append(columns, ignore_index=True)
+
+        for i in range(len(result.index)):
+            #7자리 맞춰서 앞에 0 추가
+            value = str(result['원산지 코드'][i]).zfill(7)
+            result.loc[i,'원산지 코드'] = value
+
+        result.to_excel(defPath+"result.xlsx", sheet_name="New", index=False)
+
+        # await html.browser.close()
     except:
-        await request.html.browser.close()
+        print("error")
+
+        # await html.browser.close()
 
 
+
+# randerType
+CONVERT_DB = 1
 
 if __name__ == "__main__":
-    asyncio.run(main())
-#
-#     import requests
-#     import json
-#     import re
-#
-#     target = ["title", "itemDetailUrl", "imagePath"]
-#
-#
-#     def main(url):
-#         r = requests.get(url)
-#         match = re.search(r'data: ({.+})', r.text).group(1)
-#         data = json.loads(match)
-#         goal = [data['pageModule'][x] for x in target] + \
-#                [data['priceModule']['formatedActivityPrice']]
-#         print(goal)
-#
-#
-#     main("https://es.aliexpress.com/item/32601027783.html")
+    # text()
+    asyncio.run(main(CONVERT_DB, 30, 50006675))
